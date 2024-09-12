199a1dea2ba983b02af571ec49b43eb6d73b0c79
John Mark Fuentes <jmcaluyafuentes@gmail.com>
2024-09-12 17:47:31 +1000

    refactor: add reusable function padRow that generates a row

--- pyramidGenerator.js
+++ pyramidGenerator.js
@@ -1,9 +1,18 @@
 const character = "#";
+rows = [];
 
-function pyramid (numberOfRows) {
-    for (let i = 0; i < numberOfRows; i++) {
-        console.log(" ".repeat(numberOfRows - i) + character.repeat(2 * i + 1));
+function padRow(rowNumber, rowCount) {
+    return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber + 1) + " ".repeat(rowCount - rowNumber);
+}
+
+function pyramid(count) {
+    for (let i = 0; i < count; i++) {
+        rows.push(padRow(i, count));
     }
 }
 
 pyramid(8);
+
+for (const row of rows) {
+    console.log(row);
+}
