bdc25acd4f1a36f2ca90e0c35e0f6b22c88738fb
John Mark Fuentes <jmcaluyafuentes@gmail.com>
2024-09-12 17:36:00 +1000

    chore: remove unused " ".repeat() at the end of each row

--- pyramidGenerator.js
+++ pyramidGenerator.js
@@ -2,7 +2,7 @@ const character = "#";
 
 function pyramid (numberOfRows) {
     for (let i = 0; i < numberOfRows; i++) {
-        console.log(" ".repeat(numberOfRows - i) + character.repeat(2 * i + 1) + " ".repeat(numberOfRows - i));
+        console.log(" ".repeat(numberOfRows - i) + character.repeat(2 * i + 1));
     }
 }
 
