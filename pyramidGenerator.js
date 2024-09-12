38d0acc7827ca3b005fac772bd201c28e80f6c68
John Mark Fuentes <jmcaluyafuentes@gmail.com>
2024-09-12 17:56:01 +1000

    refactor: create variable result to store all elements of rows array

--- pyramidGenerator.js
+++ pyramidGenerator.js
@@ -1,18 +1,19 @@
 const character = "#";
+count = 8;
 rows = [];
 
 function padRow(rowNumber, rowCount) {
-    return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber + 1) + " ".repeat(rowCount - rowNumber);
+    return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-function pyramid(count) {
-    for (let i = 0; i < count; i++) {
-        rows.push(padRow(i, count));
-    }
+for (let i = 1; i <= count; i++) {
+    rows.push(padRow(i, count));
 }
 
-pyramid(8);
+let result = "";
 
 for (const row of rows) {
-    console.log(row);
+    result = result + "\n" + row;
 }
+
+console.log(result);
