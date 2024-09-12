9465b5ea9fbe98394d86cf585b1728ce2b9a7c6a
John Mark Fuentes <jmcaluyafuentes@gmail.com>
2024-09-12 17:31:18 +1000

    feat: implement pyramid generator

---
+++ pyramidGenerator.js
@@ -0,0 +1,9 @@
+const character = "#";
+
+function pyramid (numberOfRows) {
+    for (let i = 0; i < numberOfRows; i++) {
+        console.log(" ".repeat(numberOfRows - i) + character.repeat(2 * i + 1) + " ".repeat(numberOfRows - i));
+    }
+}
+
+pyramid(8);
